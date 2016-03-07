b98cba5fccb2dc4df61797f610f1d5af1d9c68ba
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-03-07 04:34:38 +0400

    add documentation :)

--- 2sem/hw2/task2_1/heapsorter.h
+++ 2sem/hw2/task2_1/heapsorter.h
@@ -1,9 +1,22 @@
 #pragma once
 #include "sorter.h"
 
+/// Implementation of the sorter (heapsort).
+/*!
+    \author © Sasha Plotnikov Production, Ltd.
+
+    The child class. Inherits from Sorter.
+    The class uses a heapSort to implement overridden method sort()
+*/
+
 class HeapSorter : public Sorter
 {
 public:
+    /*!
+        This function sorts the array of elements.
+        \param array The target array
+        \param length The length of that array
+    */
     void sort(int *array, int length);
 private:
     void siftDown(int *arrayOfElements, int root, int bottom);
--- 2sem/hw2/task2_1/sorter.h
+++ 2sem/hw2/task2_1/sorter.h
@@ -1,8 +1,21 @@
 #pragma once
 
+/// Interface of the sorter.
+/*!
+    \author © Sasha Plotnikov Production, Ltd.
+
+    The parent class. Does not have implementation of methods,
+    all methods are virtual and implemented in child-classes.
+*/
+
 class Sorter
 {
 public:
+    /*!
+        This function sorts the array of elements.
+        \param array The target array
+        \param length The length of that array
+    */
     virtual void sort(int *array, int length) = 0;
     virtual ~Sorter() {}
 };
