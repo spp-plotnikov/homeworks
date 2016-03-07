32dd5024e8bc5e867a1db27a56ec66e6f8cbe831
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-03-07 05:18:43 +0400

    added documentation :D

--- 2sem/hw2/task2_2/printer.h
+++ 2sem/hw2/task2_2/printer.h
@@ -3,12 +3,33 @@
 
 using namespace std;
 
+/// prints the array in spiral order
+/*!
+    \author © Sasha Plotnikov Production, Ltd.
+
+    This Printer prints the numbers from the array of size NxN
+    in a spiral starting from the centre. N must be odd.
+    This is the parent class. It has two child classes.
+*/
+
 class Printer
 {
 public:
+    /*!
+        This method prints the elements of the array in spiral order
+        \param array Source array
+        \param size The size of the array (N)
+    */
     virtual void print(int **array, const int size) = 0;
     virtual ~Printer() {}
 protected:
+    /*!
+        This is auxiliary method.
+        \param array Source array
+        \param size The size of the array (N)
+        \param out Output stream
+    */
     void printTheArray(int **array, const int size, ostream &out) const;
+    /// This is auxiliary method.
     void printTheElements(ostream &out, int **arrayOfDigits, int start, int length) const;
 };
