1b1384011aa8da0e440df0155b591334b5600dc4
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-04-01 03:00:25 +0400

    added signal mapper and buttons for operations

--- 2sem/hw3/task3_3/mainwindow.cpp
+++ 2sem/hw3/task3_3/mainwindow.cpp
@@ -1,3 +1,4 @@
+#include <QSignalMapper>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -7,7 +8,27 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     statusBar()->showMessage("© Sasha Plotnikov Production, Ltd.");
+
+    QSignalMapper *forPrinting = new QSignalMapper(this);
+    foreach  (QPushButton *button, this->findChildren<QPushButton*>())
+    {
+        if (button->text() != "C" && button->text() != "←" && button->text() != "=")
+        {
+            forPrinting->setMapping(button, button->text());
+            connect(button, SIGNAL(clicked()), forPrinting, SLOT(map()));
+        }
+    }
+
+    connect(forPrinting, SIGNAL(mapped(const QString &)),
+        this, SLOT(printExpression(const QString &)));
 }
+
+
+void MainWindow::printExpression(const QString &text)
+{
+    ui->expression->setText(text);
+}
+
 
 MainWindow::~MainWindow()
 {
