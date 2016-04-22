7878fe14df7ff7ecd7aa642d052e530beeca2d68
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-04-22 04:43:56 +0400

    added test for task2_1

--- 2sem/hw2/task2_1/sortertest.h
+++ 2sem/hw2/task2_1/sortertest.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <QtCore/QObject>
 #include <QtTest/QtTest>
+#include <ctime>
 #include "bubblesorter.h"
 #include "quicksorter.h"
 #include "heapsorter.h"
@@ -14,7 +15,14 @@ public:
 
 
 private slots:
-    void  init1_data()
+    void init()
+    {
+        bubbleSorter = new BubbleSorter();
+        quickSorter = new QuickSorter();
+        heapSorter = new HeapSorter();
+    }
+
+    void oneElementTest_data()
     {
         QTest::addColumn<int>("type");
 
@@ -23,38 +31,128 @@ private slots:
         QTest::newRow("heap") << 2;
     }
 
-    void init1()
-    {
-        QFETCH(int, type);
-
-        enum Types {bubble, quick, heap};
-        switch (type) {
-        case bubble:
-            sorter = new BubbleSorter();
-            break;
-        case quick:
-            sorter = new QuickSorter();
-            break;
-        case heap:
-            sorter = new HeapSorter();
-            break;
-        }
-    }
-
     void oneElementTest()
     {
-        int a[10] = {};
-        a[0] = 1;
-        sorter->sort(a, 1);
+        QFETCH(int, type);
+        setCurrentType(type);
+
+        int array[1] = {1};
+        sorter->sort(array, 1);
+
+        QVERIFY(array[0] == 1);
+    }
+
+    void sortedByAscendingArrayTest_data()
+    {
+        QTest::addColumn<int>("type");
+
+        QTest::newRow("bubble") << 0;
+        QTest::newRow("quick") << 1;
+        QTest::newRow("heap") << 2;
+    }
+
+    void sortedByAscendingArrayTest()
+    {
+        QFETCH(int, type);
+        setCurrentType(type);
+
+        int array[100] = {};
+        for (int i = 0; i < 100; i++)
+        {
+            array[i] = i;
+        }
+        sorter->sort(array, 100);
+        QVERIFY(isSorted(array, 100));
+    }
+
+    void sortedInDescendingOrderTest_data()
+    {
+        QTest::addColumn<int>("type");
+
+        QTest::newRow("bubble") << 0;
+        QTest::newRow("quick") << 1;
+        QTest::newRow("heap") << 2;
+    }
+
+    void sortedInDescendingOrderTest()
+    {
+        QFETCH(int, type);
+        setCurrentType(type);
+
+        int array[100] = {};
+        for (int i = 0; i < 100; i++)
+        {
+            array[i] = 100 - i;
+        }
+        sorter->sort(array, 100);
+        QVERIFY(isSorted(array, 100));
+    }
+
+    void randomArrayTest_data()
+    {
+        QTest::addColumn<int>("type");
+
+        QTest::newRow("bubble") << 0;
+        QTest::newRow("quick") << 1;
+        QTest::newRow("heap") << 2;
+    }
+
+    void randomArrayTest()
+    {
+        QFETCH(int, type);
+        setCurrentType(type);
+
+        srand(time(NULL));
+        const int size = 200 + rand() % 10000;
+        int *array = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = rand() % 4001 - 2000;
+        }
+
+        sorter->sort(array, size);
+        bool result = isSorted(array, size);
+        delete array;
+        QVERIFY(result);
     }
 
     void cleanup()
     {
-        delete sorter;
+        delete bubbleSorter;
+        delete quickSorter;
+        delete heapSorter;
     }
 
-
-
 private:
-    Sorter *sorter;
+    void setCurrentType(int type)
+    {
+        switch (type) {
+        case bubble:
+            sorter = bubbleSorter;
+            break;
+        case quick:
+            sorter = quickSorter;
+            break;
+        case heap:
+            sorter = heapSorter;
+            break;
+        }
+    }
+
+    bool isSorted(int *array, int length)
+    {
+        bool isCorrectOrder = true;
+        for (int i = 0; i < length - 1; i++)
+        {
+            isCorrectOrder &= array[i] <= array[i + 1];
+        }
+        return isCorrectOrder;
+    }
+
+    enum Types {bubble, quick, heap};
+
+    Sorter *bubbleSorter = NULL;
+    Sorter *quickSorter = NULL;
+    Sorter *heapSorter = NULL;
+    Sorter *sorter = NULL;
 };
