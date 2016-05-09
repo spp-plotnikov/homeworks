b9109c4f5dcc696368eca691006e56665f57387d
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-05-09 19:04:39 +0400

    added tests for AVL-tree

--- 2sem/hw5/task5_2/avltreetest.h
+++ 2sem/hw5/task5_2/avltreetest.h
@@ -22,6 +22,61 @@ private slots:
         delete tree;
    }
 
+   void addTest()
+   {
+       tree->add(777);
+       QVERIFY(tree->isFound(777));
+   }
+
+   void removeTest()
+   {
+       tree->add(777);
+       tree->remove(777);
+       QVERIFY(!tree->isFound(777));
+   }
+
+   void emptyTreeArrayTest()
+   {
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
+       delete array;
+       QCOMPARE(size, 0);
+   }
+
+   void treeToArrayTest()
+   {
+       for (int i = 0; i < 42; i++)
+       {
+           tree->add(i * 7);
+       }
+
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
+
+       bool isCorrect = true;
+       for (int i = 0; i < 42; i++)
+       {
+           isCorrect &= array[i] == i * 7;
+       }
+
+       delete array;
+       QVERIFY(isCorrect);
+   }
+
+   void sizeOfArrayTest()
+   {
+       for (int i = 0; i < 42; i++)
+       {
+           tree->add(i * 4);
+       }
+
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
+       delete array;
+
+       QCOMPARE(size, 42);
+   }
+
 private:
    AVLTree<int> *tree = nullptr;
 };
