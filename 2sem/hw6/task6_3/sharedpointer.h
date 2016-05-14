0131c6b4ce7977aa4f9abb91b74cd35b0405adb6
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-05-14 04:52:19 +0400

    added two constructors

--- 2sem/hw6/task6_3/sharedpointer.h
+++ 2sem/hw6/task6_3/sharedpointer.h
@@ -5,5 +5,42 @@ template <typename Type>
 class SharedPointer
 {
 public:
-    SharedPointer();
+    SharedPointer(Type *thisObject = nullptr);
+    SharedPointer(const SharedPointer &pointer);
+    Type* operator->();
+    Type& operator*();
+    SharedPointer& operator=(const SharedPointer &pointer);
+    int getCount();
+    ~SharedPointer();
+private:
+    Type *object = nullptr;
+    int *count = nullptr;    ///<    number of copies
 };
+
+
+//------------------------------------------------------
+
+
+template <typename Type>
+SharedPointer<Type>::SharedPointer(Type *thisObject)
+{
+    object = thisObject;
+    if (object)
+    {
+        count = new int(1);
+    }
+}
+
+
+template <typename Type>
+SharedPointer<Type>::SharedPointer(const SharedPointer<Type> &pointer)
+{
+    object = pointer.object;
+    count = pointer.count;
+    if (count)
+    {
+        (*count)++;
+    }
+}
+
+TODO: 1) comments 2) tests
