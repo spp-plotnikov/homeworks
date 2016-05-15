deea699cc5b3d3daa61effb0902751ca005c7dbf
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-05-15 20:21:18 +0400

    added tests

--- 2sem/hw6/task6_3/sharedpointertest.h
+++ 2sem/hw6/task6_3/sharedpointertest.h
@@ -12,13 +12,78 @@ public:
     explicit SharedPointerTest(QObject *parent = 0) : QObject(parent) {}
 
 private slots:
-    void init()
+    void oneCopyCountTest()
     {
+        SharedPointer<int> pointer(new int(7));
+        QCOMPARE(pointer.getCount(), 1);
     }
 
-
-    void cleanup()
+    void correctPointerDereferenceTest()
     {
+        SharedPointer<int> pointer(new int(42));
+        QCOMPARE(*pointer, 42);
     }
+
+    void twoCopiesTest()
+    {
+        SharedPointer<int> pointerCopy1(new int(666));
+        SharedPointer<int> pointerCopy2(pointerCopy1);
+        QCOMPARE(pointerCopy1.getCount(), 2);
+    }
+
+    void removeCopyTest()
+    {
+        SharedPointer<int> pointerCopy1(new int(777));
+        SharedPointer<int> pointerCopy2(pointerCopy1);
+
+        pointerCopy2.~SharedPointer();
+        QCOMPARE(pointerCopy1.getCount(), 1);
+    }
+
+    void correctCopyDereferenceTest()
+    {
+        SharedPointer<int> pointerCopy1(new int(3));
+        SharedPointer<int> pointerCopy2(pointerCopy1);
+
+        QCOMPARE(*pointerCopy2, 3);
+    }
+
+    void arrowAccessOperatorTest()
+    {
+        SharedPointer<ReturnerOfSeven> pointer(new ReturnerOfSeven);
+
+        QCOMPARE(pointer->returnSeven(), 7);
+    }
+
+    void exceptionTest()
+    {
+        SharedPointer<int> nullPoinrer();
+        QVERIFY_EXCEPTION_THROWN(*nullPoinrer, SharedPointer<int>::NullPointerException);
+    }
+
+    void assignmentOperatorTest()
+    {
+        SharedPointer<int> pointer1(new int(33));
+        SharedPointer<int> poinrer2(new int(44));
+        pointer2 = poinrer1;
+        QCOMPARE(*pointer2, 33);
+    }
+
+    void nullPointerCountTest()
+    {
+        SharedPointer<int> nullPoinrer();
+
+        QCOMPARE(nullPoinrer().getCount(), 0);
+    }
+
+private:
+    class ReturnerOfSeven
+    {
+    public:
+        void returnSeven()
+        {
+            return 7;
+        }
+    };
 };
 
