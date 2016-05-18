af630dea74f370d0828bc68252f5c6db74b42d69
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-05-19 00:18:22 +0400

    added tests

--- 2sem/hw7/task7_1/avltreetest.h
+++ 2sem/hw7/task7_1/avltreetest.h
@@ -12,77 +12,120 @@ public:
    explicit AVLTreeTest(QObject *parent = 0) : QObject(parent) {}
 
 private slots:
-    TODO
-//   void init()
-//   {
-//        tree = new AVLTree<int>;
-//   }
+   void init()
+   {
+        tree = new AVLTree<int>;
+   }
 
-//   void cleanup()
-//   {
-//        delete tree;
-//   }
+   void cleanup()
+   {
+        delete tree;
+   }
 
-//   void addTest()
-//   {
-//       tree->add(tripleSeven);
-//       QVERIFY(tree->isFound(tripleSeven));
-//   }
+   void addTest()
+   {
+       tree->add(777);
+       QCOMPARE(tree->getCount(777), 1);
+       QVERIFY(tree->isFound(777));
+   }
 
-//   void removeTest()
-//   {
-//       tree->add(tripleSeven);
-//       tree->remove(tripleSeven);
-//       QVERIFY(!tree->isFound(tripleSeven));
-//   }
+   void removeTest()
+   {
+       tree->add(666);
+       tree->remove(666);
+       QCOMPARE(tree->getCount(666), 0);
+       QVERIFY(!tree->isFound(666));
+   }
 
-//   void emptyTreeArrayTest()
-//   {
-//       int size = 0;
-//       int *array = tree->toPresentContentInArray(size);
-//       delete array;
-//       QCOMPARE(size, 0);
-//   }
+   void countTest1()
+   {
+       for (int i = 0; i < 7; i++)
+       {
+           tree->add(1234);
+       }
+       QCOMPARE(tree->getCount(1234), 7);
+       QVERIFY(tree->isFound(1234));
+   }
 
-//   void treeToArrayTest()
-//   {
-//       int sevenMultiples = 0;
-//       for (int i = 0; i < 42; i++)
-//       {
-//           sevenMultiples = i * 7;
-//           tree->add(sevenMultiples);
-//       }
+   void countTest2()
+   {
+       for (int i = 0; i < 36; i++)
+       {
+           tree->add(-15);
+       }
+       for (int i = 0; i < 20; i++)
+       {
+           tree->remove(-15);
+       }
+       QCOMPARE(tree->getCount(-15), 16);
+       QVERIFY(tree->isFound(-15));
+   }
 
-//       int size = 0;
-//       int *array = tree->toPresentContentInArray(size);
+   void emptyTreeArrayTest()
+   {
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
+       delete array;
+       QCOMPARE(size, 0);
+   }
 
-//       bool isCorrect = true;
-//       for (int i = 0; i < 42; i++)
-//       {
-//           isCorrect &= array[i] == i * 7;
-//       }
+   void treeToArrayTest1()
+   {
+       for (int i = 0; i < 42; i++)
+       {
+           tree->add(i * 7);
+       }
 
-//       delete array;
-//       QVERIFY(isCorrect);
-//   }
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
 
-//   void sizeOfArrayTest()
-//   {
-//       int fourMultiples = 0;
-//       for (int i = 0; i < 42; i++)
-//       {
-//           fourMultiples = i * 4;
-//           tree->add(fourMultiples);
-//       }
+       bool isCorrect = true;
+       for (int i = 0; i < 42; i++)
+       {
+           isCorrect &= array[i] == i * 7;
+       }
 
-//       int size = 0;
-//       int *array = tree->toPresentContentInArray(size);
-//       delete array;
+       delete array;
+       QVERIFY(isCorrect);
+   }
 
-//       QCOMPARE(size, 42);
-//   }
+   void treeToArrayTest2()
+   {
+       int expectedUnsorted[7] = {14, 10, 10, 84, 10, 83, 84};
+       int expectedSorted[7] = {10, 10, 10, 14, 83, 84, 84};
 
-//private:
-//   AVLTree<int> *tree = nullptr;
-//   const int tripleSeven = 777;
+       for (int i = 0; i < 7; i++)
+       {
+           tree->add(expectedUnsorted[i]);
+       }
+
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
+
+       bool isCorrect = true;
+       for (int i = 0; i < 7; i++)
+       {
+           isCorrect &= array[i] == expectedSorted[i];
+       }
+
+       delete array;
+       QVERIFY(isCorrect);
+   }
+
+   void sizeOfArrayTest()
+   {
+       for (int i = 0; i < 42; i++)
+       {
+           tree->add(i * 4);
+       }
+
+       int size = 0;
+       int *array = tree->toPresentContentInArray(size);
+       delete array;
+
+       QCOMPARE(size, 42);
+   }
+
+private:
+   AVLTree<int> *tree = nullptr;
 };
--- 2sem/hw7/task7_1/bagtest.h
+++ 2sem/hw7/task7_1/bagtest.h
@@ -1,1 +1,100 @@
-TODO
+#pragma once
+
+#include <QtCore/QObject>
+#include <QtTest/QtTest>
+#include "bag.h"
+
+
+class BagTest : public QObject
+{
+   Q_OBJECT
+public:
+   explicit BagTest(QObject *parent = 0) : QObject(parent) {}
+
+private slots:
+   void init()
+   {
+        bag = new Bag;
+   }
+
+   void cleanup()
+   {
+        delete bag;
+   }
+
+   void addTest()
+   {
+       bag->add(777);
+       QCOMPARE(bag->getCount(777), 1);
+       QVERIFY(bag->find(777));
+   }
+
+   void removeTest()
+   {
+       bag->add(666);
+       bag->remove(666);
+       QCOMPARE(bag->getCount(666), 0);
+       QVERIFY(!bag->find(666));
+   }
+
+   void countTest1()
+   {
+       for (int i = 0; i < 7; i++)
+       {
+           bag->add(1234);
+       }
+       QCOMPARE(bag->getCount(1234), 7);
+       QVERIFY(bag->find(1234));
+   }
+
+   void countTest2()
+   {
+       for (int i = 0; i < 36; i++)
+       {
+           bag->add(-15);
+       }
+       for (int i = 0; i < 20; i++)
+       {
+           bag->remove(-15);
+       }
+       QCOMPARE(bag->getCount(-15), 16);
+       QVERIFY(bag->find(-15));
+   }
+
+   void getContentTest()
+   {
+       for (int i = 0; i < 42; i++)
+       {
+           bag->add(i * 7);
+       }
+
+       int size = 0;
+       int *array = bag->getContent(size);
+
+       bool isCorrect = true;
+       for (int i = 0; i < 42; i++)
+       {
+           isCorrect &= array[i] == i * 7;
+       }
+
+       delete array;
+       QVERIFY(isCorrect);
+   }
+
+   void sizeOfArrayTest()
+   {
+       for (int i = 0; i < 42; i++)
+       {
+           bag->add(i * 4);
+       }
+
+       int size = 0;
+       int *array = bag->getContent(size);
+       delete array;
+
+       QCOMPARE(size, 42);
+   }
+
+private:
+   Bag *bag = nullptr;
+};
