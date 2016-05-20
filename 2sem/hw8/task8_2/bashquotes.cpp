2fffdff472e93afc11c842cbbbfb3b65f33f03da
Alexandr Plotnikov <Sasha-Plotnikov-Production@yandex.ru>
2016-05-21 02:57:08 +0400

    added reloading function

--- 2sem/hw8/task8_2/bashquotes.cpp
+++ 2sem/hw8/task8_2/bashquotes.cpp
@@ -6,15 +6,71 @@ BashQuotes::BashQuotes(QWidget *parent) :
     ui(new Ui::BashQuotes)
 {
     ui->setupUi(this);
+    setFixedSize(420, 370);
+
     ui->increaseRating->setIcon(QIcon(":new/prefix1/images/plus.png"));
     ui->decreaseRating->setIcon(QIcon(":new/prefix1/images/minus.png"));
     ui->bayan->setIcon(QIcon(":new/prefix1/images/bayan2.png"));
-    ui->load->setIcon(QIcon(":new/prefix1/images/load.png"));
+    ui->load->setIcon(QIcon(":new/prefix1/images/reload2.png"));
     ui->next->setIcon(QIcon(":new/prefix1/images/next1.png"));
     statusBar()->showMessage("© Sasha Plotnikov Production, Ltd.");
+
+    webView->load(QUrl("http://bash.im/"));
+
+    connect(webView, SIGNAL(loadFinished(bool)), this, SLOT(loadQuotes()));
+    connect(ui->next, SIGNAL(clicked()), this, SLOT(nextQuote()));
+    connect(ui->load, SIGNAL(clicked()), this, SLOT(reloadQuotes()));
 }
 
 BashQuotes::~BashQuotes()
 {
+    delete webView;
+
     delete ui;
+}
+
+
+//----------------------------------------------------------
+
+
+void BashQuotes::loadQuotes()
+{
+    if (!ui->next->isEnabled())
+    {
+        quotesBlocks = webView->page()->mainFrame()->findAllElements("div[class=quote]").toList();
+        quoteIterator = --quotesBlocks.end();
+        nextQuote();
+        ui->next->setEnabled(true);
+    }
+}
+
+
+void BashQuotes::nextQuote()
+{
+    QString textOfQuote;
+    do
+    {
+        quoteIterator++;
+        if (quoteIterator == quotesBlocks.end())
+        {
+            quoteIterator = quotesBlocks.begin();
+        }
+        textOfQuote = quoteIterator->findFirst("div[class=text]").toPlainText();
+    }
+    while (textOfQuote.isEmpty());
+
+    ui->quote->clear();
+    ui->quote->insertPlainText(textOfQuote);
+    ui->rating->setText(quoteIterator->findFirst("span[class=rating-o]").toPlainText());
+}
+
+
+void BashQuotes::reloadQuotes()
+{
+    int i = quotesBlocks.indexOf(*quoteIterator);
+    webView->reload();
+    quotesBlocks = webView->page()->mainFrame()->findAllElements("div[class=quote]").toList();
+    quoteIterator = quotesBlocks.begin();
+    quoteIterator += (i % quotesBlocks.size());
+    ui->rating->setText(quoteIterator->findFirst("span[class=rating-o]").toPlainText());
 }
--- 2sem/hw8/task8_2/bashquotes.h
+++ 2sem/hw8/task8_2/bashquotes.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QWebElement>
+#include <QWebFrame>
+#include <QWebView>
+
 
 namespace Ui {
 class BashQuotes;
@@ -14,6 +18,18 @@ public:
     explicit BashQuotes(QWidget *parent = 0);
     ~BashQuotes();
 
+private slots:
+    void nextQuote();
+    void loadQuotes();
+    void reloadQuotes();
+//    void rateUp();
+//    void rateDown();
+//    void rateBayan();
+
 private:
     Ui::BashQuotes *ui;
+
+    QWebView *webView = new QWebView();
+    QList<QWebElement> quotesBlocks;
+    QList<QWebElement>::iterator quoteIterator;
 };
